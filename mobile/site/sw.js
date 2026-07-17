2817e6fcb0eb56d8e0486be81af6bfe50093e7fd
HattedPuppet <hattedpuppet@gmail.com>
2026-07-17 17:27:44 +0900

    fix: refresh mobile app cache

--- mobile/site/sw.js
+++ mobile/site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cg-signal-mobile-v11";
+const CACHE_NAME = "cg-signal-mobile-v12";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
